6e043bb9e4fcc018ee1c60088f69ca6d2b02330f
dadeLarsen <github@dadelarsen.com>
2025-11-04 20:05:05 -0600

    removed some template code and changed titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Hearth Hub",
+  description: "A small-scale real estate management app",
 };
 
 const geistSans = Geist({
